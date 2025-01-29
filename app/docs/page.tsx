bef6171275d21769a578efc62d5ad3e5cc448cfa
Celeteck <theoheneba24@gmail.com>
2025-01-29 23:17:07 +0000

    Changed type to search placeholder text

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -61,7 +61,7 @@ export default function AsyncExample() {
       options={options}
       value={selected}
       onChange={setSelected}
-      placeholder="Type to search...."
+      placeholder="Type to search......"
     />
   )
 }`
